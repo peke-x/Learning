9b7bbed5d3e7c6dc136c5b99d32fa087ee97af3f
peke-x <oolong-t@gol.com>
2017-06-06 00:37:14 +0900

    Learning
    Self study programing.

---
+++ src/Data.java
@@ -0,0 +1,21 @@
+
+public class Data {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//int a = 2, b = 3;
+		//a = a * b;
+
+		byte a = 127;
+		short b = 10000;
+		float c = 1.2f;
+		double d = 0.34;
+
+		//System.out.println(a);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
---
+++ src/Print.java
@@ -0,0 +1,14 @@
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 10;
+		double b = 3.24;
+		System.out.printf("%10d\n", a);
+		System.out.printf("x %8.5f\n", b);
+		System.out.printf("----------\n");
+		System.out.printf("%10f\n\n", a * b);
+	}
+
+}
